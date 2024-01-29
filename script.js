7eb79a823544331e8bdcce5eae0980fbf041f96e
maniel <manuelpepr99@icloud.com>
2024-01-29 20:15:37 +0000

    create playRound function that takes two param function is supposed to compare options by player and computer and return a string saying either a win, a tie or a lose
    used an if else if condition for this one and also
    player option should be case insensitive so i used a toLowerCase()
    for the solution
    Note

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-let options = [ 'rocks', 'paper', 'scissors']
+let options = [ 'rock', 'paper', 'scissors']
 
 let random = Math.floor(Math.random()* options.length)
-console.log(options[random])
+
 
 function getComputerChoice (){
-    return random
+    return options[random]
 }
+
+
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+    return " You Win! Rock beats Scissors "
+   }
+   else if( playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'){
+    return " You Lose! Paper beats Rock "
+   }
+   else if ( playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
+    return " Its a tie"
+   }
+   else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+    return " You Win! Paper beats Rock"
+   }
+   else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper'){
+    return " Its a tie"
+   }
+   else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+    return " You Lose! Scissors beats Paper"
+   }
+   else if ( playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock'){
+    return " You lose! Rock beats scissors"
+   }
+   else if ( playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'){
+    return " You Win! Scissors beats Paper"
+   }
+   else if ( playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors'){
+    return " Its a tie"
+   }
+}
+
+const playerSelection = 'Rock';
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log( computerSelection);
+console.log(playRound(playerSelection, computerSelection))
