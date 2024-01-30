d4869185e90be08e45cf209757240598f223da7a
maniel <manuelpepr99@icloud.com>
2024-01-30 22:16:01 +0000

    fix the getComputerchoice

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-let options = [ 'rock', 'paper', 'scissors']
+let computerPlay = [ 'rock', 'paper', 'scissors'];
 
-let random = Math.floor(Math.random()* options.length)
+
 
 
 function getComputerChoice (){
-    return options[random]
+        let random = Math.floor(Math.random()* computerPlay.length);
+        return computerPlay[random];
 }
 
+const playerSelection = 'Rock';
 
 function playRound(playerSelection, computerSelection) {
+
    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
     return " You Win! Rock beats Scissors "
+
    }
    else if( playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'){
     return " You Lose! Paper beats Rock "
    }
    else if ( playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
     return " Its a tie"
-   }
+   }//
    else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
     return " You Win! Paper beats Rock"
    }
@@ -38,8 +42,16 @@ function playRound(playerSelection, computerSelection) {
    }
 }
 
-const playerSelection = 'Rock';
-const computerSelection = getComputerChoice();
-console.log(playerSelection);
-console.log( computerSelection);
-console.log(playRound(playerSelection, computerSelection))
+
+function playGame() {
+    for (let i = 0; i < 5;i++ ){
+       let computerSelection = getComputerChoice();
+       console.log(playRound(playerSelection, computerSelection))
+    }
+
+  
+}
+
+
+
+playGame()
