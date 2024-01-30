9f6c9e95f0f747cd4a9cc65059f2ab735fce7d96
maniel <manuelpepr99@icloud.com>
2024-01-30 22:37:15 +0000

    add score tracking feature to program

--- script.js
+++ script.js
@@ -8,33 +8,42 @@ function getComputerChoice (){
         return computerPlay[random];
 }
 
-const playerSelection = 'Rock';
+let  playerSelection = 'rock'
+playerScore = 0;
+computerScore = 0;
+
 
 function playRound(playerSelection, computerSelection) {
 
    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
-    return " You Win! Rock beats Scissors "
+        playerScore += 1;
+    return " You Win! Rock beats Scissors ";
 
    }
    else if( playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'){
+        computerScore += 1
     return " You Lose! Paper beats Rock "
    }
    else if ( playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
     return " Its a tie"
    }//
    else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+    playerSelection += 1
     return " You Win! Paper beats Rock"
    }
    else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper'){
     return " Its a tie"
    }
    else if ( playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+    computerScore += 1
     return " You Lose! Scissors beats Paper"
    }
    else if ( playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock'){
+    computerScore += 1
     return " You lose! Rock beats scissors"
    }
    else if ( playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'){
+    playerScore += 1
     return " You Win! Scissors beats Paper"
    }
    else if ( playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors'){
@@ -46,9 +55,17 @@ function playRound(playerSelection, computerSelection) {
 function playGame() {
     for (let i = 0; i < 5;i++ ){
        let computerSelection = getComputerChoice();
+      
        console.log(playRound(playerSelection, computerSelection))
+       console.log(` player: ${ playerScore} pc: ${ computerScore}`)
+       console.log( ' ')
     }
-
+    if ( playerScore > computerScore){
+        console.log(`You win with ${playerScore} points`)
+    }else {
+        console.log(`Computer wins with ${ computerScore} points`)
+    }
+    
   
 }
 
